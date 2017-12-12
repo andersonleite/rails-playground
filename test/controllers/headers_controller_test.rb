cb776ba7ea07b6a4fb13868ce3f6f6b3e6f70142
Anderson Leite <leitea@google.com>
2017-12-11 19:39:01 -0800

    Working on test for a Feed api.

--- test/controllers/headers_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HeadersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get headers_show_url
-    assert_response :success
-  end
-
-end
